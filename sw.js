809aeca6351dbf0b3a491b85cae3ffea381ec471
auccelis-cmd <auccelis@gmail.com>
2026-09-22 00:47:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='capture-pad-v131';
+const CACHE='capture-pad-v132';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
